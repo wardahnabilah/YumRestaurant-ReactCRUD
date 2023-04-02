27d7b4d951a62333ab93884bbbd150617c04ee09
unknown <wardahnabilah33@gmail.com>
2023-04-02 14:25:37 +0700

    Edit OrderForm Component

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,18 +1,29 @@
+import { useState } from 'react';
 import './OrderForm.css';
 
-export default function OrderForm() {
+export default function OrderForm(props) {
+    
+    // Create state to save the input value
+    
+
+    // Function to set the inputValue when the input changed
+    
+
+    // Function to pass inputValue to App.js
+    
+    
     return(
-        <div className="container container--order">
+        <div className="container--order">
             <h1>Place Your Order</h1>
             <div className="form">
-                <form>
+                <form autoComplete="off">
                     <div className="form__item--table">
                         <h2>Table No.</h2>
-                        <input className="form__input" type="text"/>
+                        <input className="form__input" type="text" name="tableNumber"/>
                     </div>
-                    <input className="form__input" type="text" name="customer-name" placeholder="Customer Name" />
-                    <input className="form__input" type="text" name="detail-order" placeholder="Your Order" />
-                    <input className="form__input form__input--btn" type="submit" />
+                    <input className="form__input" type="text" name="customerName" placeholder="Customer Name" />
+                    <textarea className="form__input" type="textarea" rows="6" cols="22" name="detailOrder" placeholder="Your Order" />
+                    <button className="form__input form__input--btn" type="submit"> Add Order </button>
                 </form>
             </div>
         </div>
