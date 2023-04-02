b263fe8a1cde4f39fb5c9542fd4e38164f3d6a16
unknown <wardahnabilah33@gmail.com>
2023-04-02 12:33:59 +0700

    Add order filled component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import OrderForm from "./components/OrderForm"
+import OrderFilled from "./components/OrderFilled"
 
 export default function App() {
     return (
         <div>
             <Header />
             <OrderForm />
+            <OrderFilled />
             <Footer />
         </div>
     )
