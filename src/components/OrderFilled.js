088c3579acb1e0cc88f73e2f0bdc2feb59c1ed69
unknown <wardahnabilah33@gmail.com>
2023-04-02 14:24:33 +0700

    Edit OrderFilled Component

--- src/components/OrderFilled.js
+++ src/components/OrderFilled.js
@@ -2,16 +2,16 @@ import './OrderFilled.css';
 
 export default function OrderFilled() {
     return(
-        <div className="container container--order">
+        <div className="container--filled">
             <div className="form form--filled">
                 <i className="icon__edit bx bxs-message-square-edit"></i>
                 <i className="icon__delete bx bxs-trash-alt"></i>
-                <h2>Table No. 1</h2> 
+                <h3>Table 1</h3> 
                 <div className="filled__item">
-                    <p className="bold">Customer Name: <span>James</span></p>
+                    <p className="filled__item--name">Customer Name: <span>James</span></p>
                 </div>
                 <div className="filled__item">
-                    <p className="bold">Detail Order:</p>
+                    <p className="filled__item--detail">Detail Order:</p>
                     <li>Pizza, fried rice</li>
                 </div>
             </div>
