cb9059b0668ca7cf5e3ec742fb2c337d0c37eebe
unknown <wardahnabilah33@gmail.com>
2023-04-07 18:42:24 +0700

    Fix things

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,16 @@ export default function Header() {
     return(
         <header className="container">
             <div className="header-logo">
-                <a href="#"><img src={Logo} alt="" /></a>
+                <a href=""><img src={Logo} alt="" /></a>
             </div>
             <div className="toggle-button">
-                <i class='bx bx-menu'></i>
+                <i className='bx bx-menu'></i>
             </div>
             <nav>
                 <ul className='nav__list'>
-                    <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
-                    <li className="nav__item"><a href="#" className="nav__link">Menu</a></li>
-                    <li className="nav__item"><a href="#" className="nav__link">Place Order</a></li>
+                    <li className="nav__item"><a href="" className="nav__link">Home</a></li>
+                    <li className="nav__item"><a href="" className="nav__link">Menu</a></li>
+                    <li className="nav__item"><a href="" className="nav__link">Place Order</a></li>
                 </ul>
             </nav>
         </header>
